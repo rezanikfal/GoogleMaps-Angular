333e0e0f03856e61f31b2c40bc7a88f75c251127
Reza Nikfal <mr.nikfal@gmail.com>
2020-05-04 21:30:13 -0500

    Load Google Maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Inject, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'GoogleMap';
+  showButton = true
+  open() {
+    this.showButton = false
+    let map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 3,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
 }
